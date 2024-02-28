f36358dc5809f8be19254509cea404854dbc0ff7
irsanrais <irsanrais94@gmail.com>
2024-02-28 14:31:41 +0700

    add page and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/about";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import Signin from "./pages/signin";
+import Signup from "./pages/signup";
 
 export default function App() {
-  return <div>BPHL V</div>;
+  return;
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
+      <Route path="/Projects" element={<Projects />} />
+      <Route path="/Sign-In" element={<Signin />} />
+      <Route path="/Sign-Up" element={<Signup />} />
+    </Routes>
+  </BrowserRouter>;
 }

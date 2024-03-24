d01d566970695bcdfed71be775c0820b04523d53
irsanrais <irsanrais94@gmail.com>
2024-03-24 22:40:49 +0700

    add sign up page

--- client/component/Header.jsx
+++ client/component/Header.jsx
@@ -11,7 +11,7 @@ export default function Header() {
         to="/"
         className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white"
       >
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-blac">
+        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
           BPHL V
         </span>
         Blog
@@ -32,9 +32,7 @@ export default function Header() {
           <FaMoon />
         </Button>
         <Link to="/sign-in">
-          <Button gradientDuoTone="purpleToBlue" outline>
-            Sign In
-          </Button>
+          <Button gradientDuoTone="purpleToBlue">Sign In</Button>
         </Link>
         <Navbar.Toggle />
       </div>
@@ -42,10 +40,10 @@ export default function Header() {
         <Navbar.Link active={path === "/"} as={"div"}>
           <Link to="/">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/About"} as={"div"}>
+        <Navbar.Link active={path === "/about"} as={"div"}>
           <Link to="/about">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === "/Projects"} as={"div"}>
+        <Navbar.Link active={path === "/projects"} as={"div"}>
           <Link to="/projects">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
